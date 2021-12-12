7e1f89cc37a4fbbc6e6fc01ee3875b7f4f106170
Kyzoid <kyzoid@gmail.com>
2021-12-12 18:10:51 +0000

    Deploying to gh-pages from @ Kyzoid/osu-activity@9b72d33d71905daa17533a3dd74b07dc0066b21f 🚀

--- _nuxt/static/1639254866/rankings/mania/country-first-places/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1639254866",layout:"default",error:a,state:{authUser:a,types:{}},serverRendered:true,routePath:"\u002Frankings\u002Fmania\u002Fcountry-first-places",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
---
+++ _nuxt/static/1639332647/rankings/mania/performance/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1639332647",layout:"default",error:a,state:{authUser:a,types:{}},serverRendered:true,routePath:"\u002Frankings\u002Fmania\u002Fperformance",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
